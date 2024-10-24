153b04daba09af797ca2ea6148e436f2883f805e
Shihab k s <neethu@luminartechnolab.com>
2024-10-24 09:53:45 +0530

    api integration with factory objects

--- lib/controller/facts_screen_controller.dart
+++ lib/controller/facts_screen_controller.dart
@@ -1,12 +1,9 @@
-import 'dart:convert';
-import 'dart:developer';
-
 import 'package:cat_facts_api_sample_integration/models/facts_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class FactsScreenController with ChangeNotifier {
-  FactsModel? resObj;
+  CatFactResmodel? resObj;
   bool isLoading = false;
   Future<void> getFact() async {
     isLoading = true;
@@ -17,15 +14,7 @@ class FactsScreenController with ChangeNotifier {
       var respose = await http.get(url);
 
       if (respose.statusCode == 200) {
-        print("data fetched");
-        print(respose.body);
-
-        var convertedjson = jsonDecode(respose.body);
-
-        resObj = FactsModel(
-          fact: convertedjson["fact"],
-          length: convertedjson["length"],
-        );
+        resObj = catFactResmodelFromJson(respose.body);
       }
     } catch (e) {
       print(e);
--- lib/models/facts_model.dart
+++ lib/models/facts_model.dart
@@ -1,6 +1,29 @@
-class FactsModel {
-  String? fact;
-  num? length;
+// class FactsModel {
+//   String? fact;
+//   num? length;
 
-  FactsModel({this.fact, this.length});
+//   FactsModel({this.fact, this.length});
+// }
+
+import 'dart:convert';
+
+CatFactResmodel catFactResmodelFromJson(String jsondata) {
+  return CatFactResmodel.fromJson(json.decode(jsondata));
+}
+
+class CatFactResmodel {
+  String? fact;
+  int? length;
+
+  CatFactResmodel({
+    this.fact,
+    this.length,
+  });
+
+  factory CatFactResmodel.fromJson(Map<String, dynamic> convertedData) {
+    return CatFactResmodel(
+      fact: convertedData["fact"],
+      length: convertedData["length"],
+    );
+  }
 }
